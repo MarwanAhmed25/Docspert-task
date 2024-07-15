09b8c6aa8933cbbb32bb138c3fbcdfbc0a93cc07
marwan <marwan4125881@gmail.com>
2024-07-15 05:40:09 +0300

    create accounts templates and view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import account_list
+from .views import account_list, account_detail
 
 app_name = 'accounts'
 urlpatterns = [
     path('', account_list, name='account_list'),
+    path('/<uuid:pk>', account_detail, name='account_detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Account, Transaction
 # Create your views here.
 def account_list(request):
@@ -6,4 +7,14 @@ def account_list(request):
         accounts = Account.objects.filter(name__icontains=request.GET.get('search'))
     else:
         accounts = Account.objects.all()
-    return render(request, 'accounts/list.html', {'accounts': accounts})
+    return render(request, 'accounts/list.html', {'accounts': accounts, 'page':'home'})
+
+def account_detail(request, pk):
+    account = Account.objects.get(pk=pk)
+    if request.GET.get('search'):
+        transactions = Transaction.objects.filter(Q(sender__name__icontains=request.GET.get('search')&
+                         Q(receiver=account))|Q(receiver__name__icontains=request.GET.get('search')& Q(sender=account)))
+    else:
+        transactions = Transaction.objects.filter(Q(sender=account)|Q(receiver=account))
+
+    return render(request, 'accounts/detail.html', {'account': account, 'transactions': transactions, 'page':'account_detail'})
