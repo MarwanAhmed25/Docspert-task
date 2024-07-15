286442889138ae5d9010ad330b5c30d5aae9c748
marwan <marwan4125881@gmail.com>
2024-07-15 10:49:31 +0300

    valid balance error message

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,10 +12,4 @@ class TransactionForm(forms.ModelForm):
         model = Transaction
         fields = ['sender', 'receiver', 'amount']
 
-        def clean_amount(self):
-            print('hello')
-            # check the balance validation for sender account
-            if self.cleaned_data['amount'] == 0:
-                return 0
-            return 1
 
