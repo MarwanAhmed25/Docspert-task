c1cc0092c16aeef71d45308f53ab42e5bdd82018
marwan <marwan4125881@gmail.com>
2024-07-15 04:08:50 +0300

    create accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import uuid
+from django.core.validators import MinValueValidator
 # Create your models here.
+class Account(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    balance = models.FloatField(default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_transactions')
+    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receiver_transactions')
+    amount = models.FloatField(default=0, validators=[MinValueValidator(0)])
+    created_in = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
