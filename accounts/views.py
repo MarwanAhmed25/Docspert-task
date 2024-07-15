7ad90480056cab056af23dcd50ba157a8784e4d7
marwan <marwan4125881@gmail.com>
2024-07-15 05:03:53 +0300

    create accounts admin panel

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from .models import Account, Transaction
 # Create your views here.
 def account_list(request):
-    accounts = Account.objects.all()
+    if request.GET.get('search'):
+        accounts = Account.objects.filter(name__icontains=request.GET.get('search'))
+    else:
+        accounts = Account.objects.all()
     return render(request, 'accounts/list.html', {'accounts': accounts})
